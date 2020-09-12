dd377e045bcde59fcc92cbc33dce9d15891890d0
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2020-09-12 12:18:44 -0400

    Add base file

---
+++ test.py
@@ -0,0 +1,112 @@
+import tkinter
+import os
+import sys
+from sys import platform as _plt
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import tkinter.simpledialog as tksd
+
+# Here, we are creating our class, Window, and inheriting from the Frame
+# class. Frame is a class from the tkinter module. (see Lib/tkinter/__init__)
+class Window(Frame):
+
+    # Define settings upon initialization. Here you can specify
+    def __init__(self, master=None):
+        
+        # parameters that you want to send through the Frame class. 
+        Frame.__init__(self, master)   
+
+        #reference to the master widget, which is the tk window                 
+        self.master = master
+
+        #with that, we want to then run init_window, which doesn't yet exist
+        self.init_window()
+
+    #Creation of init_window
+    def init_window(self):
+
+        # changing the title of our master widget      
+        self.master.title("Shutdown Helper")
+
+        # allowing the widget to take the full space of the root window
+        self.pack(fill=BOTH, expand=YES)
+        # creating a button instance
+        lable_choose = StringVar()
+        quitButton = Button(self, text="Cancel", command=self.client_exit)
+        shutDown = Button(self, text="Shut Down", command=self.turnoff)
+        reStart = Button(self, text="Restart", command=self.rebootcomputer)
+        logOff = Button(self, text="Logoff", command=self.logoffuser)
+        label = Label(self, textvariable=lable_choose, compound=RIGHT)
+        lockScreen = Button(self, text="Lock", command=self.lockscreen)
+        lablethesecond = Label(self, text="You can Shut down, Restart, or Log off.", compound=RIGHT)
+
+        # placing the button on my window
+        buttons = quitButton, shutDown, reStart, logOff, lockScreen
+        lable_choose.set("Goodbye, "+ os.getlogin()+". Are you sure you are done with this computer?")
+        label.pack()
+        lablethesecond.pack()
+        shutDown.pack(side=LEFT, anchor=W, fill=X, expand=YES)#place(x=253, y=42)
+        reStart.pack(side=LEFT, anchor=W, fill=X, expand=YES)#place(x=207, y=42)
+        lockScreen.pack(side=LEFT, anchor=W, fill=X, expand=YES)#place(x=10, y=42)
+        logOff.pack(side=LEFT, anchor=W, fill=X, expand=YES)#place(x=90, y=42)
+        quitButton.pack(side=LEFT, anchor=W, fill=X, expand=YES)#place(x=148, y=42)
+
+
+       
+    def turnoff(self):
+        if _plt == "linux" or _plt == "linux2":
+            os.system("shutdown now")
+        elif _plt == "darwin":
+            userpasswd = tksd.askstring("Password", "[sudo] password for "+os.getlogin()+":", show='*')
+            os.system("echo "+userpasswd+" | sudo -S shutdown -h")
+        elif _plt == "win32":
+            os.system("shutdown.exe /s")
+        elif _plt == "win64":
+            os.system("shutdown.exe /s")
+    def rebootcomputer(self):
+        if _plt == "linux" or _plt == "linux2":
+            os.system("shutdown -r now")
+        elif _plt == "darwin":
+            userpasswd = tksd.askstring("Password", "[sudo] password for "+os.getlogin()+":", show='*')
+            os.system("echo "+userpasswd+"| sudo -S shutdown -r now")
+        elif _plt == "win32":
+            os.system("shutdown.exe /r")
+        elif _plt == "win64":
+            os.system("shutdown.exe /r")
+    def logoffuser(self):
+        if _plt == "linux" or _plt == "linux2":
+            userpasswd = tksd.askstring("Password", "[sudo] password for "+os.getlogin()+":", show='*')
+            os.system("echo "+userpasswd+" | sudo -S skill -KILL -u "+os.getlogin())
+        elif _plt == "darwin":
+            messagebox.showerror("Error", "Could not log off user.")
+        elif _plt == "win32":
+            os.system("shutdown /l")
+        elif _plt == "win64":
+            os.system("shutdown /l")
+    def lockscreen(self):
+        if _plt == "linux" or _plt == "linux2":
+            userpasswd = tksd.askstring("Password", "[sudo] password for "+os.getlogin()+":", show='*')
+            os.system("echo "+userpasswd+" | sudo -S apt install gnome-screensaver && gnome-screensaver-command -l && gnome-screensaver-command -a")
+        elif _plt == "darwin":
+            os.system("/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend")
+        elif _plt == "win32":
+            os.system("rundll32.exe user32.dll,LockWorkStation")
+        elif _plt == "win64":
+            os.system("rundll32.exe user32.dll,LockWorkStation")
+    def client_exit(self):
+        exit()
+
+
+# root window created. Here, that would be the only window, but
+# you can later have windows within windows.
+root = Tk()
+
+root.geometry("400x80")
+
+root.resizable(width=False, height=False)
+
+#creation of an instance
+app = Window(root)
+
+root.mainloop()  
